7d7e5b0927ef3bd051630061bcbb60c5f14bdb4e
Bernadette Nakazibwe <118615206+Bernadettenakazibwe@users.noreply.github.com>
2024-02-01 11:25:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,31 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Set the secret key for the session
+# Setting  the secret key for the session
 app.config['SECRET_KEY'] = '22211788'
 
-# Configure the SQLite database
+# Configuring  the SQLite database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
 db = SQLAlchemy(app)
 
-# Define the Book model
+# Defining  the Book model
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     author = db.Column(db.String(255), nullable=False)
     publication_year = db.Column(db.Integer)
 
-# Create the database tables
+# I am creating  the database tables
 with app.app_context():
     db.create_all()
 
-# Route to display the list of books
+# IrRoute to display the list of books
 @app.route('/books')
 def books():
     book_list = Book.query.all()
     return render_template('books.html', books=book_list)
 
-# Route to add a new book
+#I gain route to add a new book
 @app.route('/add_book', methods=['GET', 'POST'])
 def add_book():
     if request.method == 'POST':
@@ -36,19 +36,19 @@ def add_book():
         author = request.form['author']
         publication_year = request.form['publication_year']
 
-        # Create a new book instance
+        # I am creating a new book instance
         new_book = Book(title=title, author=author, publication_year=publication_year)
 
-        # Add the book to the database
+        # Adding  the book to the database
         db.session.add(new_book)
         db.session.commit()
 
-        # Redirect to the list of books
+        # Redirecting  to the list of books
         return redirect(url_for('books'))
 
     return render_template('add_book.html')
 
-# Close the database connection after each request
+# Closing the database connection after each request
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db.session.remove()
